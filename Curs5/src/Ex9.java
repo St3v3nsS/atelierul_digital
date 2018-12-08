e0d6dbfcecbc2f1d528343a00090f0b53788f205
St3v3nsS <ionut.morosan@outlook.com>
2018-12-08 13:21:11 +0200

    Curs 5 -Philosophers Problem ERR

--- Curs5/src/Ex9.java
+++ Curs5/src/Ex9.java
@@ -6,6 +6,7 @@ public class Ex9 {
         Monitor m = new Monitor();
         m.initialization_code();
 
+        // create random threads for philosophers
 
         int cnt = 0;
         int val = 10;
@@ -32,10 +33,11 @@ public class Ex9 {
         @Override
         public void run() {
             //System.out.println(number + " " + monitor.getState(number));
+            // Check if the philosopher i is Eating
             if(!monitor.getState(number).equals(states.EATING))
-            {
+            {   //if not, try to eat
                 monitor.pickup(number);
-
+                // put down the chopsticks
                 monitor.putdown(number);
             }
         }
@@ -59,15 +61,15 @@ public class Ex9 {
 
         synchronized void pickup(int i){
             state[i] = states.HUNGRY;
-            test(i);
-            while (!state[i].equals(states.EATING)) callWaitMethod();
+            test(i);// check for available chopsticks
+            while (!state[i].equals(states.EATING)) callWaitMethod(); //wait until it finds
         }
 
         synchronized void test(int i){
-            if( !state[(i + 4) % 5].equals(states.EATING) &&
-                !state[(i + 1) % 5].equals(states.EATING) &&
-                state[i].equals(states.HUNGRY)){
-                state[i] = states.EATING;
+            if( !state[(i + 4) % 5].equals(states.EATING) && // check the left neighbour
+                !state[(i + 1) % 5].equals(states.EATING) && // check the right one
+                state[i].equals(states.HUNGRY)){  // and your are hungry
+                state[i] = states.EATING;    // eat
                 System.out.println("EATING " + i);
                 try {
                     Thread.sleep(500);
@@ -76,14 +78,14 @@ public class Ex9 {
                     e.printStackTrace();
                 }
 
-                notify();
+                notify();   // signal yourself that you are not hungry anymore
             }
         }
 
         synchronized void putdown(int i){
-            state[i] = states.THINKING;
+            state[i] = states.THINKING; // done eating, start thinking
             System.out.println("Done EATING " + i + " neighbours: " + (i + 4) % 5 + " " +(i + 1) % 5 + " with states: " + state[(i + 4) % 5] + " " + state[(i + 1) % 5]);
-            test((i+4)%5);
+            test((i+4)%5);  // test the neighbours
             test((i+1)%5);
         }
 
